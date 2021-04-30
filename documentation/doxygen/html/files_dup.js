14f4021ab4d366c9a2758ee96deee4008b090a4c
Chinnam-Narendra-Prasad <82199636+Chinnam-Narendra-Prasad@users.noreply.github.com>
2021-04-30 22:05:26 +0530

    Delete documentation/doxygen directory

--- documentation/doxygen/html/files_dup.js
+++
@@ -1,7 +0,0 @@
-var files_dup =
-[
-    [ "adc.h", "adc_8h_source.html", null ],
-    [ "pwm.h", "pwm_8h_source.html", null ],
-    [ "seatandbutton.h", "seatandbutton_8h_source.html", null ],
-    [ "usart.h", "usart_8h_source.html", null ]
-];
